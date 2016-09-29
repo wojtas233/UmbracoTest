bbcec37613a908f4d80ea1afe12dfd059e069ad7
wnowak <w.nowak19@gmail.com>
2016-09-29 14:31:53 +0200

    Initialize umbraco test project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UmbracoTest.Startup))]
+namespace UmbracoTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
